27a82283d06d6a410ea7610d6e739f6b71272b40
Sameer <sameer.writes.70@gmail.com>
2022-08-21 21:03:49 +0530

    dsa

--- practice/singlell_ins_end.java
+++ practice/singlell_ins_end.java
@@ -1,40 +1,37 @@
-
 package practice;
 
-import java.util.*;
+import java.util.Scanner;
 
-import javax.lang.model.element.Element;
+public class singlell_ins_end {
 
-public class singlell_ins_beg {
-
-    class Node {
+    static class Node {
         int data;
         Node next;
 
-        Node(int data) {
+        Node(int data){
             this.data = data;
-            this.next = next;
-        }
+             this.next = next;
+                    }
     }
 
     Node head = null;
 
     void creation() {
         int data, n;
+
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.println("Enter The Data ");
+            System.out.println("Enter the data");
             data = sc.nextInt();
             Node new_node = new Node(data);
             if (head == null) {
                 head = new_node;
             } else {
-                
-                System.out.println("enter 0 to insert the data at the begining");
                 new_node.next = head;
                 head = new_node;
+
             }
-            System.out.println("press 1 for continue");
+            System.out.println("Press 1 for continue");
             n = sc.nextInt();
         } while (n == 1);
 
@@ -44,20 +41,19 @@ public class singlell_ins_beg {
         Node temp = head;
         if (head == null) {
             System.out.println("Linked List Does Not exists");
-        }
-
-        else {
+        } else {
+            // Node temp = head;
             while (temp != null) {
-                System.out.println(temp.data + " ");
                 temp = temp.next;
             }
+            Node new_node;
+            temp.next! = new_node;
         }
     }
 
     public static void main(String[] args) {
-        singlell_ins_beg ll = new singlell_ins_beg();
+        singlell_ins_end ll = new singlell_ins_end();
         ll.creation();
         ll.traverse();
     }
-
 }
